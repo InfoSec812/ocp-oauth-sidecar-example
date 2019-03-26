1c851ced0c790cbc8a206e5c75ad79a46566be6a
Deven Phillips <deven.phillips@gmail.com>
2019-03-26 08:03:21 -0400

    Added REST call and rendering to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,37 @@
 <template>
   <q-page padding>
-    <p>This is an about page</p>
+    <ul v-if="noErrors">
+      <li v-for="item in oapiStructure.resources" :key="item.name">
+        {{ item.kind }}
+      </li>
+    </ul>
+    <div v-if="!noErrors">
+      An error occurred retrieving information from the OpenShift API
+    </div>
   </q-page>
 </template>
 
 <script>
+import Axios from 'axios';
+
 export default {
   name: 'PageAbout',
+  data: {
+    oapiStructure: {},
+    noErrors: true,
+  },
+  beforeCreate: () => {
+    axios.get("/oapi/v1")
+        .then((response) => {
+          if (response.status == 200) {
+            this.$data.oapiStructure = response.data;
+          } else {
+            this.$data.oapiStructure = {}
+          }
+        })
+        .catch((error) => {
+          this.$data.noErrors = false;
+        });
+  }
 };
 </script>
